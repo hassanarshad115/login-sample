d09ad9159dcf8483ab3cbbbd64ba4d6918f0e10d
hassan <hassanarshad115@gmail.com>
2017-09-20 14:45:03 -0500

    Add project files.

---
+++ sample/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernametextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordtextBox = new System.Windows.Forms.TextBox();
+            this.loginbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // usernametextBox
+            // 
+            this.usernametextBox.Location = new System.Drawing.Point(160, 24);
+            this.usernametextBox.Name = "usernametextBox";
+            this.usernametextBox.Size = new System.Drawing.Size(183, 20);
+            this.usernametextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // passwordtextBox
+            // 
+            this.passwordtextBox.Location = new System.Drawing.Point(160, 72);
+            this.passwordtextBox.Name = "passwordtextBox";
+            this.passwordtextBox.Size = new System.Drawing.Size(183, 20);
+            this.passwordtextBox.TabIndex = 1;
+            // 
+            // loginbutton
+            // 
+            this.loginbutton.Location = new System.Drawing.Point(160, 146);
+            this.loginbutton.Name = "loginbutton";
+            this.loginbutton.Size = new System.Drawing.Size(75, 23);
+            this.loginbutton.TabIndex = 2;
+            this.loginbutton.Text = "Login";
+            this.loginbutton.UseVisualStyleBackColor = true;
+            this.loginbutton.Click += new System.EventHandler(this.loginbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 302);
+            this.Controls.Add(this.loginbutton);
+            this.Controls.Add(this.passwordtextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.usernametextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox usernametextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passwordtextBox;
+        private System.Windows.Forms.Button loginbutton;
+    }
+}
+
---
+++ sample/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loginbutton_Click(object sender, EventArgs e)
+        {
+            if (IsValidated())
+            {
+                bool usernamev, passwordv;
+
+                GetAuthenticated(out usernamev, out passwordv);
+                {
+
+                    if (usernamev && passwordv)
+                    {
+                        MessageBox.Show("Successfully Login");
+                    }
+                    else
+                    {
+                        if (!usernamev)
+                        {
+                            MessageBox.Show("username not correct");
+                        }
+                        else
+                        {
+                            MessageBox.Show("password not correct");
+                        }
+                    }
+
+
+
+
+
+
+                }
+            }
+        }
+        //private void GetAuthenticated(out bool usernamev, out bool passwordv)
+
+        private bool GetAuthenticated(out bool usernamev, out bool passwordv)
+        {
+            string connctionstring = ConfigurationManager.ConnectionStrings["t"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connctionstring);
+            SqlCommand cmd = new SqlCommand("sp", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            conn.Open();
+
+            cmd.Parameters.Add("@isusername", SqlDbType.Bit).Direction = ParameterDirection.Output;
+            cmd.Parameters.Add("@ispassword", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+            cmd.Parameters.AddWithValue("@username", usernametextBox.Text);
+            cmd.Parameters.AddWithValue("@password", passwordtextBox.Text);
+            cmd.ExecuteNonQuery();
+
+
+            usernamev =(bool)cmd.Parameters["@isusername"].Value;
+            passwordv = (bool)cmd.Parameters["@ispassword"].Value;
+
+            return true;
+
+        }
+
+
+
+
+
+
+        private bool IsValidated()
+        {
+            if (usernametextBox.Text.Trim() == string.Empty)
+            {
+               MessageBox.Show("username is required you cannot empty this");
+                return false;
+            }
+            if (passwordtextBox.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("password is required you cannot empty this");
+                return false;
+            }
+            return true;
+        }
+    }
+}
